4e6c586d6a668c7ade03d9b9bcc916235163e19f
Verified-p <verifieddeveloper13@gmail.com>
2026-09-09 07:49:36 +0300

    kucsa

--- kucsa_platform/settings.py
+++ kucsa_platform/settings.py
@@ -92,6 +92,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
 
+    # Serve collected static files in production
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
 
     "django.middleware.common.CommonMiddleware",
